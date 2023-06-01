e9422120559ddef103ab4cbc5764ccdc20aa7875
YrinaSydorko <rozaidoma@gmail.com>
2023-06-01 17:02:13 +0300

    task11

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import fetchApiImages from './js/api.js';
+import { refs, apiOptions } from './js/api.js';
+
+refs.form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  refs.gallary.innerHTML = '';
+  refs.btnMore.classList.add('none');
+  apiOptions.page = 1;
+
+  await fetchApiImages(apiOptions.API_KEY, apiOptions.page);
+});
+
+refs.btnMore.addEventListener('click', async () => {
+  apiOptions.page += 1;
+
+  await fetchApiImages(apiOptions.API_KEY, apiOptions.page);
+});
+
+
---
+++ src/js/api.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export const refs = {
+  gallary: document.querySelector('.gallery'),
+  inputSearch: document.querySelector('input[name="searchQuery"]'),
+  form: document.querySelector('#search-form'),
+  btnMore: document.querySelector('.load-more'),
+};
+
+export const apiOptions = {
+  API_KEY: '36447145-713fd0865ba966cc8244c878c',
+  page: 1,
+};
+
+function renderMarkupImages(data) {
+  const card = data.hits
+    .map(el => {
+      return `<div class="photo-card">
+        <a href="${el.largeImageURL}"><img width="350" height="200" src="${el.webformatURL}" alt="${el.tags}" loading="lazy" /></a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${el.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${el.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${el.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${el.downloads}
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+
+  return card;
+}
+
+export default async function fetchApiImages(API_KEY, page) {
+  await axios
+    .get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${refs.inputSearch.value}&per_page=40&page=${page}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(({ data }) => {
+      refs.gallary.insertAdjacentHTML('beforeend', renderMarkupImages(data));
+
+      refs.btnMore.classList.remove('none');
+      new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+
+      if (data.totalHits === 0) {
+        refs.btnMore.classList.add('none');
+
+        Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+
+        return;
+      }
+
+      if (data.hits.length < 40) {
+        refs.btnMore.classList.add('none');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+
+        return;
+      }
+    })
+    .catch(error => Notify.failure('Failure! not found api'));
+}
